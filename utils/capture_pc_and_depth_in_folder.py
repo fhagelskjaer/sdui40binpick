9ede72964c5d99f72506734c331ac9ffa3dfd400
Frederik Hagelskjær <frederikhagel@gmail.com>
2024-10-29 13:14:40 +0100

    utils/capture_pc_and_depth_in_folder.py changed settings_file to param

--- utils/capture_pc_and_depth_in_folder.py
+++ utils/capture_pc_and_depth_in_folder.py
@@ -25,18 +25,13 @@ def _point_cloud_to_cv_z(point_cloud):
     return depth_map_uint16
 
 class ZividPcCam:
-    def __init__(self):
+    def __init__(self, settings_file = "/home/fhagelskjaer/nov13_spec.yml"):
         app = zivid.Application()
 
         print("Connecting to camera")
         self.camera = app.connect_camera()
 
         print("Configuring settings")
-        # self.settings = zivid.Settings(acquisitions=[zivid.Settings.Acquisition(aperture=fnum) for fnum in (11.31, 5.66, 2.83)])
-        # settings_file = "/home/fhagelskjaer/camera_settings_10_sec.yml"
-        # settings_file = "/home/fhagelskjaer/nov13_diffuse.yml"
-        settings_file = "/home/fhagelskjaer/nov13_spec.yml"
-        
         self.settings = zivid.Settings.load(settings_file)
 
         camera_intrinsics = calibration.intrinsics(self.camera)
@@ -47,16 +42,11 @@ class ZividPcCam:
 
         self.newcameramtx, roi = cv2.getOptimalNewCameraMatrix(self.mtx,self.dist,(self.width,self.height),0, (self.width,self.height))
 
-
     def compute(self):
         print("Capturing frame (HDR)")
         with self.camera.capture(self.settings) as frame:
             point_cloud = frame.point_cloud()
 
-        #data_file = "Frame.zdf"
-        #with zivid.Frame(data_file) as frame:
-        #    point_cloud = frame.point_cloud()
-        
         depth_map = _point_cloud_to_cv_z(point_cloud)
 
         xyz = point_cloud.copy_data("xyz")
@@ -69,8 +59,6 @@ class ZividPcCam:
         point_cloud_open3d = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(xyz))
         point_cloud_open3d.colors = o3d.utility.Vector3dVector(rgb_vector / 255)
 
-        # if normals is not None:
-            # point_cloud_open3d.normals = o3d.utility.Vector3dVector(normals)
         return point_cloud_open3d, depth_map, rgb
 
     def camera_matrix(self):
@@ -84,7 +72,7 @@ def main():
 
     while True:
 
-        input_string = input("write_name output folder name: ('q' to exit)\n") 
+        input_string = input("write output folder name: ('q' to exit)\n") 
 
         if input_string == 'q':
             break
